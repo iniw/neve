53070f4b18db3ad8967b03f3f2444210b19b8b1b
Vinicius Deolindo <git@vini.cat>
2026-05-18 23:33:24 -0300

    Client: Make `chat` rpc requests and observe responses

--- crates/neve-client/src/main.rs
+++ crates/neve-client/src/main.rs
@@ -1,7 +1,14 @@
 use anyhow::bail;
-use neve_protocol::{SERVER_PORT, ShareInfoRequest, neve_service_client::NeveServiceClient};
+use neve_protocol::{
+    ChatRequest, SERVER_PORT, ShareInfoRequest, neve_service_client::NeveServiceClient,
+};
 use std::env::args;
-use tracing::info;
+use tokio::{
+    io::{AsyncBufReadExt, BufReader, stdin},
+    sync::mpsc,
+};
+use tokio_stream::{StreamExt, wrappers::ReceiverStream};
+use tracing::{debug, error, info};
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
 #[tokio::main]
@@ -19,6 +26,35 @@ async fn main() -> anyhow::Result<()> {
     let request = tonic::Request::new(ShareInfoRequest { name });
 
     _ = client.share_info(request).await?;
+
+    let (tx, rx) = mpsc::channel(128);
+
+    tokio::spawn(async move {
+        let mut stdin = BufReader::new(stdin());
+        loop {
+            let mut buf = String::new();
+            match stdin.read_line(&mut buf).await {
+                Ok(0) => break,
+                Ok(_) => {
+                    buf.pop();
+                    _ = tx.send(buf).await;
+                }
+                Err(err) => {
+                    error!(?err, "Error reading stdin");
+                }
+            }
+        }
+    });
+
+    let requests = ReceiverStream::new(rx).map(|message| ChatRequest { message });
+
+    let mut responses = client.chat(requests).await?.into_inner();
+
+    while let Some(response) = responses.next().await {
+        if let Ok(response) = response {
+            debug!(?response, "Got a response");
+        }
+    }
 
     Ok(())
 }
