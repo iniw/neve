15c53e53b7bd36345a86fcfbdf23aa24a2699852
Vinicius Deolindo <git@vini.cat>
2026-08-25 18:45:11 -0300

    Server: Deny duplicate rows in `chat_account`
    This prevents the same account from "being in a chat" multiple times,
    which doesn't make logical sense.
    
    This could be triggered by a client calling `CreateChat` with itself
    as a participant.

--- server/migrations/20260821234954_chat.sql
+++ server/migrations/20260821234954_chat.sql
@@ -7,5 +7,7 @@ CREATE TABLE chat (
 
 CREATE TABLE chat_account (
   chat_id int NOT NULL REFERENCES chat ON DELETE CASCADE,
-  account_id int NOT NULL REFERENCES account ON DELETE CASCADE
+  account_id int NOT NULL REFERENCES account ON DELETE CASCADE,
+  -- An account can only be in a chat once
+  UNIQUE (chat_id, account_id)
 );
