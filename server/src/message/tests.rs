479135c4d91143bd527390cfe14e9742cf78f307
Vinicius Deolindo <git@vini.cat>
2026-08-31 10:39:17 -0300

    Server: Improve `MessageServer`'s tests

--- server/src/message/tests.rs
+++ server/src/message/tests.rs
@@ -1,6 +1,5 @@
-use std::collections::HashSet;
-
 use crate::{auth::AuthServer, chat::ChatServer};
+use futures::{TryStreamExt, stream::FuturesUnordered};
 use neve_proto::server::v1::{
     CreateChatRequest, CreateChatResponse, chat_service_server::ChatService,
 };
@@ -27,16 +26,6 @@ async fn get_message(db: PgPool) -> anyhow::Result<()> {
         .await?
         .into_inner();
 
-    let mut responses = server
-        .get_past_messages(Request::new(GetPastMessagesRequest { chat_id }))
-        .await?
-        .into_inner();
-
-    assert!(
-        responses.next().await.is_none(),
-        "No messages have been sent yet"
-    );
-
     let content = "Hello!".to_owned();
 
     let SendMessageResponse { message_id } = server
@@ -58,15 +47,6 @@ async fn get_message(db: PgPool) -> anyhow::Result<()> {
     assert_eq!(response.account_id, vini);
     assert_eq!(response.chat_id, chat_id);
     assert_eq!(response.content, content);
-
-    let mut responses = server
-        .get_past_messages(Request::new(GetPastMessagesRequest { chat_id }))
-        .await?
-        .into_inner();
-
-    let response = responses.next().await.expect("A message was just sent")?;
-
-    assert_eq!(response.message_id, message_id);
 
     Ok(())
 }
@@ -166,23 +146,24 @@ async fn get_future_messages(db: PgPool) -> anyhow::Result<()> {
         .await?
         .into_inner();
 
-    for message in ["Hello!", "Bye!"] {
-        let content = message.to_owned();
-
-        let SendMessageResponse { message_id } = server
-            .send_message(AuthInfo::request_from(
+    let sent_messages = ["Hello!", "Yes!", "Bye!"]
+        .into_iter()
+        .map(|message| {
+            server.send_message(AuthInfo::request_from(
                 vini,
                 SendMessageRequest {
                     chat_id,
-                    content: content.clone(),
+                    content: message.to_owned(),
                 },
             ))
-            .await?
-            .into_inner();
+        })
+        .collect::<FuturesUnordered<_>>()
+        .try_collect::<Vec<_>>()
+        .await?;
 
+    for sent_message in sent_messages {
         let response = responses.next().await.expect("A message was just sent")?;
-
-        assert_eq!(response.message_id, message_id);
+        assert_eq!(response.message_id, sent_message.get_ref().message_id);
     }
 
     Ok(())
@@ -208,17 +189,19 @@ async fn get_messages(db: PgPool) -> anyhow::Result<()> {
         .await?
         .into_inner();
 
-    let content = "Past".to_owned();
-    let SendMessageResponse { message_id } = server
-        .send_message(AuthInfo::request_from(
-            vini,
-            SendMessageRequest {
-                chat_id,
-                content: content.clone(),
-            },
-        ))
-        .await?
-        .into_inner();
+    let past_messages = (0..16)
+        .map(|index| {
+            server.send_message(AuthInfo::request_from(
+                vini,
+                SendMessageRequest {
+                    chat_id,
+                    content: format!("Past {index}"),
+                },
+            ))
+        })
+        .collect::<FuturesUnordered<_>>()
+        .try_collect::<Vec<_>>()
+        .await?;
 
     let mut responses = server
         .get_messages(Request::new(GetMessagesRequest { chat_id }))
@@ -226,82 +209,29 @@ async fn get_messages(db: PgPool) -> anyhow::Result<()> {
         .into_inner();
 
     // We get messages from the past
-    let response = responses.next().await.expect("A message was just sent")?;
-    assert_eq!(response.message_id, message_id);
+    for message in past_messages {
+        let response = responses.next().await.expect("A message was just sent")?;
+        assert_eq!(response.message_id, message.get_ref().message_id);
+    }
 
-    // And from the future
-    for message in ["Hello!", "Bye!"] {
-        let content = message.to_owned();
-
-        let SendMessageResponse { message_id } = server
-            .send_message(AuthInfo::request_from(
+    let future_messages = (0..16)
+        .map(|index| {
+            server.send_message(AuthInfo::request_from(
                 vini,
                 SendMessageRequest {
                     chat_id,
-                    content: content.clone(),
+                    content: format!("Message {index}"),
                 },
             ))
-            .await?
-            .into_inner();
+        })
+        .collect::<FuturesUnordered<_>>()
+        .try_collect::<Vec<_>>()
+        .await?;
 
+    // And from the future
+    for message in future_messages {
         let response = responses.next().await.expect("A message was just sent")?;
-        assert_eq!(response.message_id, message_id);
-    }
-
-    Ok(())
-}
-
-#[sqlx::test]
-async fn get_messages_concurrent_sends(db: PgPool) -> anyhow::Result<()> {
-    let auth_server = AuthServer::for_tests(db.clone());
-    let chat_server = ChatServer::new(db.clone());
-    let server = MessageServer::new(db).await?;
-
-    let AuthInfo { account_id: vini } = auth_server.generate_authenticated_account().await?;
-    let AuthInfo { account_id: julia } = auth_server.generate_authenticated_account().await?;
-
-    let CreateChatResponse { chat_id } = chat_server
-        .create_chat(AuthInfo::request_from(
-            vini,
-            CreateChatRequest {
-                participants: vec![julia],
-                name: None,
-            },
-        ))
-        .await?
-        .into_inner();
-
-    let mut responses = server
-        .get_messages(Request::new(GetMessagesRequest { chat_id }))
-        .await?
-        .into_inner();
-    let mut other_responses = server
-        .get_messages(Request::new(GetMessagesRequest { chat_id }))
-        .await?
-        .into_inner();
-
-    let sent_message_ids = futures::future::try_join_all((0..16).map(|index| {
-        server.send_message(AuthInfo::request_from(
-            vini,
-            SendMessageRequest {
-                chat_id,
-                content: format!("Message {index}"),
-            },
-        ))
-    }))
-    .await?
-    .into_iter()
-    .map(|response| response.into_inner().message_id)
-    .collect::<HashSet<_>>();
-
-    for responses in [&mut responses, &mut other_responses] {
-        let mut received_message_ids = HashSet::with_capacity(sent_message_ids.len());
-        for _ in 0..sent_message_ids.len() {
-            let response = responses.next().await.expect("A message was just sent")?;
-            assert!(received_message_ids.insert(response.message_id));
-        }
-
-        assert_eq!(received_message_ids, sent_message_ids);
+        assert_eq!(response.message_id, message.get_ref().message_id);
     }
 
     Ok(())
